105a8704fd468ccff7b3b5d8389439b4bba55181
Doğa Yıldız <dogasuyildizz@gmail.com>
2025-03-09 20:58:52 +0300

    Sorting algoritmalarından 1 soru çözüldü.

---
+++ CASortingAlgorithms/CASortingAlgorithms/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace CASortingAlgorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Selection Sort
+
+            //int[] sayilar = { 10, 2, 8, 1, 5, 7, 9, 3, 6, 4 };
+
+            //for (int i = 0; i < sayilar.Length - 1; i++)
+            //{
+            //    int minIndeks = i;
+            //    for (int j = i + 1; j < sayilar.Length; j++)
+            //    {
+            //        if (sayilar[j] < sayilar[minIndeks])
+            //            minIndeks = j;
+            //    }
+            //    int temp = sayilar[i];
+            //    sayilar[i] = sayilar[minIndeks];
+            //    sayilar[minIndeks] = temp;
+
+            //}
+            //foreach (int sayi in sayilar)
+            //{
+            //    Console.Write(sayi + " ");
+            //}
+
+
+            #endregion
+            #region MyRegion
+
+            #endregion
+        }
+    }
+}

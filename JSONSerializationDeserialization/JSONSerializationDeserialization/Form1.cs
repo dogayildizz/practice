a1ee686d5ef0756b3e2ac6cdee8a7780e5fd829c
Doğa Yıldız <dogasuyildizz@gmail.com>
2025-03-16 11:31:24 +0300

    Güncelleme seçeneği eklendi, hata düzeltmeleri yapıldı.

--- JSONSerializationDeserialization/JSONSerializationDeserialization/Form1.cs
+++ JSONSerializationDeserialization/JSONSerializationDeserialization/Form1.cs
@@ -8,6 +8,46 @@ namespace JSONSerializationDeserialization
     {
         List<Kisi> kisiler = new List<Kisi>();
 
+        //Silme ve güncelleme iþlemlerini list üzerinde yaptýk. Sonrasýnda bu list ile kisiler.txt yi ve datagridviewi güncelledik.
+
+        public void TextDosyasiniVeDataGridViewiGuncelle()
+        {
+            //önceki kisiler dosyamýzý sildik. sonrasýnda yeni listedeki bilgilerle yeni bir kisiler dosyasý oluþturduk.
+            File.Delete("Kisiler.txt");
+            using (StreamWriter yazici = new StreamWriter("Kisiler.txt", true))
+            {
+                foreach (Kisi kisi in kisiler)  //Yeni listeyi tekrardan Serialize ederek kisiler.txt dosyasýna yazdýrdýk.
+                {
+                    yazici.WriteLine(JsonSerializer.Serialize(kisi));
+                }
+            }
+
+            dgvKisiler.DataSource = null;  //dgv ye de yeni listeyi yazdýrdýk.
+            dgvKisiler.DataSource = kisiler;
+        }
+        public void KisiGuncelle()
+        {
+            Kisi secilenKisi = new Kisi();
+            secilenKisi.Ad = dgvKisiler.SelectedRows[0].Cells[0].Value.ToString();
+            secilenKisi.Soyad = dgvKisiler.SelectedRows[0].Cells[1].Value.ToString();
+            secilenKisi.Adres = dgvKisiler.SelectedRows[0].Cells[2].Value.ToString();
+
+            for (int i = 0; i < kisiler.Count; i++)  //Listede kullanýcýnýn seçtiðiyle ayný bilgilere sahip olan kiþiyi bulup yeni deðerlerini içine atadýk.
+            {
+                Kisi kisi = kisiler[i];
+                if (kisi.Ad == secilenKisi.Ad && kisi.Soyad == secilenKisi.Soyad && kisi.Adres == secilenKisi.Adres)
+                {
+                    kisi.Ad = txtAd.Text;
+                    kisi.Soyad = txtSoyad.Text;
+                    kisi.Adres  = txtAdres.Text;
+                }
+            }
+            //Listedeki kiþi güncellendi.
+            TextDosyasiniVeDataGridViewiGuncelle();
+            MessageBox.Show("Kiþi baþarýyla güncellendi!");
+            TextBoxlariTemizle();
+
+        }
         public void KisiSil()
         {
             //kullanýcýnýn seçtiði satýrdaki bilgileri secilenKisi adýndaki yeni Kisi nesnesine attým.
@@ -25,22 +65,11 @@ namespace JSONSerializationDeserialization
                     kisiler.RemoveAt(i);
                 }
             }
-            //önceki kisiler dosyamýzý sildik. sonrasýnda yeni listedeki bilgilerle yeni bir kisiler dosyasý oluþturduk.
-            File.Delete("Kisiler.txt");
-            using (StreamWriter yazici = new StreamWriter("Kisiler.txt", true))
-            {
-                foreach (Kisi kisi in kisiler)  //Yeni listeyi tekrardan Serialize ederek kisiler.txt dosyasýna yazdýrdýk.
-                {
-                    yazici.WriteLine(JsonSerializer.Serialize(kisi));
-                }
-            }
-
-            dgvKisiler.DataSource = null;  //dgv ye de yeni listeyi yazdýrdýk.
-            dgvKisiler.DataSource = kisiler;
+            TextDosyasiniVeDataGridViewiGuncelle();
             MessageBox.Show("Kiþi baþarýyla silindi!");
-            Temizle();
+            TextBoxlariTemizle();
         }
-        public void Temizle()
+        public void TextBoxlariTemizle()
         {
             txtAd.Text = txtSoyad.Text = txtAdres.Text = string.Empty;
         }
@@ -49,26 +78,25 @@ namespace JSONSerializationDeserialization
 
             bool mevcutMu = false;
 
-
-            //foreach (Kisi kisi in kisiler)  //Kiþiler listesinde ayný isim ve soyisimde bir var mý kontrol edildi.
-            //{
-            //    if (kisi.Ad.ToLower() == isim.ToLower() && kisi.Soyad.ToLower() == soyisim.ToLower())
-            //    {
-            //        MessageBox.Show("Bu isim ve soyisimde biri zaten mevcut!");
-            //        mevcutMu = true;
-            //        break;
-            //    }
-            //}            
+            foreach (Kisi kisi in kisiler)  //Kiþiler listesinde ayný isim ve soyisimde bir var mý kontrol edildi.
+            {
+                if (kisi.Ad.ToLower() == isim.ToLower() && kisi.Soyad.ToLower() == soyisim.ToLower())
+                {
+                    MessageBox.Show("Bu isim ve soyisimde biri zaten mevcut!");
+                    mevcutMu = true;
+                    TextBoxlariTemizle();
+                    break;
+                }
+            }
             return mevcutMu;
 
 
         }
         public void DosyadanGetir()  //kisiler.txt dosyasýndan aldýðý satýrlarý deserialize edip listeye ekliyor.
         {
-            if (!File.Exists("Kisiler.txt")) //Kisiler.txt varsa
-                return;
+
             kisiler.Clear();
-            using (StreamReader okuyucu = new StreamReader("Kisiler.txt"))
+            using (StreamReader okuyucu = new StreamReader("Kisiler.txt", true))
             {
                 string satir = okuyucu.ReadLine();
 
@@ -97,7 +125,7 @@ namespace JSONSerializationDeserialization
 
             MessageBox.Show("Baþarýyla eklenmiþtir.");
 
-            Temizle();
+            TextBoxlariTemizle();
             DosyadanGetir();
         }
         public Form1()
@@ -108,26 +136,35 @@ namespace JSONSerializationDeserialization
         private void btnEkle_Click(object sender, EventArgs e)
         {
             //eðer kiþi mevcutsa metot kýrýlýr, kaydetme iþlemi yapýlmaz.
-            if (!string.IsNullOrEmpty(txtSoyad.Text) && !string.IsNullOrEmpty(txtSoyad.Text) && BuKisiMevcutMu(txtAd.Text, txtSoyad.Text))
+            if (!string.IsNullOrEmpty(txtAd.Text) && !string.IsNullOrEmpty(txtSoyad.Text) && BuKisiMevcutMu(txtAd.Text, txtSoyad.Text))
                 return;
-
+            if(string.IsNullOrEmpty(txtAd.Text)|| string.IsNullOrEmpty(txtSoyad.Text)||string.IsNullOrEmpty(txtAdres.Text) )
+            {
+                MessageBox.Show("Lütfen tüm alanlarý doldurduðunuzdan emin olun!");
+                return;
+            }
             Kaydet();
-            Temizle();
+            TextBoxlariTemizle();
         }
 
         private void btnSil_Click(object sender, EventArgs e)
         {
-            if(dgvKisiler.SelectedRows.Count == 0)
+            if (dgvKisiler.SelectedRows.Count == 0)
             {
                 MessageBox.Show("Lütfen bir satýr seçiniz.");
                 return;
             }
-                
+
             KisiSil();
         }
         private void btnGuncelle_Click(object sender, EventArgs e)
         {
-
+            if (dgvKisiler.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Lütfen bir satýr seçiniz.");
+                return;
+            }
+            KisiGuncelle();
         }
 
         private void dgvKisiler_CellClick(object sender, DataGridViewCellEventArgs e)
